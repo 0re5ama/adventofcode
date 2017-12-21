6a793b00b2f5e8ef0eb388ae9f3f6e9fdcfc7680
0re5ama <shahbibek10@gmail.com>
2017-12-21 08:41:55 +0545

    eslint

--- 03/a.js
+++ 03/a.js
@@ -2,10 +2,10 @@ var spiral = [[0,0]];
 const final = 312051;
 
 const dir = Object.freeze({
-  right: 0,
-  up: 1,
-  left: 2,
-  down: 3,
+    right: 0,
+    up: 1,
+    left: 2,
+    down: 3,
 });
 
 var count = 1;
@@ -15,19 +15,19 @@ const condition = _ => {
 };
 
 const forward = _ => {
-  curPos = spiral[spiral.length-1];
-  spiral.push(1);
+    curPos = spiral[spiral.length-1];
+    spiral.push(1);
 };
 
 const turnLeft = _ => {
-  curDir ++;
-  curDir %= 4;
+    curDir ++;
+    curDir %= 4;
 };
 
 while(spiral.length < final) {
-  if (condition)
-    forward();
-  else
-    turnLeft();
-  console.log(spiral);
+    if (condition)
+        forward();
+    else
+        turnLeft();
+    console.log(spiral);
 }
